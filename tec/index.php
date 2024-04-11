8de54da6219d62de77e7a56471569cd6a4abae02
Evgueny <evgueny@diatomenterprises.com>
2024-04-11 14:43:32 +0300

    eee
    test

--- tec/index.php
+++ tec/index.php
@@ -1,5 +1,5 @@
 <title> Internet audio diagnostika priekš tec</title>
 
 <h1> Internet audio diagnostika priekš tec</h1>
-<a href="tec.html">TEC Client</a><BR>
+<a href="tec.html">TEC Client 2</a><BR>
 <a href="server/index.html">TEC SERVER emulator</a><BR>
